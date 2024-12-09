0c4c951a3e36e5209a07a94564e09fcd63c41bd6
EDUISGALEAS <ei.galeasv@uea.edu.ec>
2024-12-08 23:24:15 -0500

    Update README.md
    Ejemplos

---
+++ UNIDAD 1/1.2 Tecnicas de Programacion.py
@@ -0,0 +1,15 @@
+class persona:
+    #constructor
+    def __unit__ (self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.nombre = apellido
+        self.edad = edad
+    def saludar(self):
+        print("Hola mi nombre es")
+
+#instanciar objeto
+
+persona = Persona( nombre: "EDU", apellido: "galeas" edad "35")
+print(persona.nombre)
+print(persona.apellido)
+print(persona.edad)
